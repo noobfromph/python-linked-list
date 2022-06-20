48075995a7b1ec177075f1becd7e39b408dc8c65
Ej <elton@meldcx.com>
2022-06-20 17:08:47 +0800

    added setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_linked_list',
+    version='0.1',
+    license='GNU General Public License v3.0',
+    author="Ej Lungay",
+    author_email='ejlunday@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/noobfromph/python-linked-list.git',
+    keywords='python linked list',
+    install_requires=[]
+)
--- shared/node.py
+++
@@ -1,9 +0,0 @@
-class node:
-    """
-    Represents a node. 
-    A node is an object that has a data that is pointing to another node.
-    """
-    data = None
-    next_node = None
-    def __init__(self, data):
-        self.data = data
